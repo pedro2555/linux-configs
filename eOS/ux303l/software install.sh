c90ec0c0333504c0e114ce9a64d9b419ffa81eb3
Pedro Rodrigues <prodrigues@ux303-prodrigues.lan>
2015-09-20 14:54:04 +0100

    Basic software installations and ux303l configurations

---
+++ eOS/ux303l/software install.sh
@@ -0,0 +1,53 @@
+#
+# PPAs
+#
+# Braseo
+sudo add-apt-repository ppa:renbag/ppa
+# TLP
+sudo add-apt-repository ppa:linrunner/tlp
+# eOS tweaks
+sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
+
+sudo apt-get update
+
+#
+# Installations
+#
+
+# Productivity
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install giggle
+
+# Web
+sudo apt-get install chromium-browser
+
+# Media
+sudo apt-get install vlc
+
+# Utilities
+# Disks utility
+sudo apt-get install gnome-disk-utility
+# System monitor app
+sudo apt-get install gnome-system-monitor
+# CD/DVD burner
+sudo apt-get install brasero
+# Backup settings
+sudo apt-get install deja-dup
+# Commonly used libraries
+sudo apt-get install ubuntu-restricted-extras
+# Battery reduce
+sudo apt-get install tlp tlp-rdw
+# eOS tweaks
+sudo apt-get install elementary-tweaks
+# uGet download manager
+sudo apt-get install uget
+# virtualization suite
+sudo apt-get install virtualbox
+
+#
+# Configurations
+#
+sudo tlp start
+
+
---
+++ eOS/ux303l/ux303-config.sh
@@ -0,0 +1,37 @@
+#
+# PPAs
+#
+
+# Touchpad
+sudo add-apt-repository ppa:hanipouspilot/focaltech-dkms
+# Brightness indicator
+sudo add-apt-repository ppa:indicator-brightness/ppa
+
+# update and upgrade
+sudo apt-get update
+sudo apt-get upgrade
+
+#
+#  Install Packages
+#
+
+# vivid's kernel, for touchpad drives
+sudo apt-get install linux-generic-lts-vivid
+# Focaltech's touchpad dirver
+sudo apt-get install focaltech-dkms
+# brightness indicator
+sudo apt-get install indicator-brightness
+# backlight
+# xrandr --output eDP1 --brightness (.0-1)
+# CPU freq, for performance-powersave options
+sudo apt-get install indicator-cpufreq
+
+#
+# Configurations
+#
+# enable Bluetooth
+rfkill unblock bluetooth
+# Ads blocking
+sudo wget http://winhelp2002.mvps.org/hosts.txt --output-document=/etc/hosts
+
+
