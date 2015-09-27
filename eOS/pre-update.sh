a13dbbccc03bf907881c0e47ebfa761732936848
Pedro Rodrigues <prodrigues1990@gmail.com>
2015-09-27 21:32:27 +0100

    Revised install script, add pre-upgrade, pre-install, and opost-install scripts

---
+++ eOS/pre-update.sh
@@ -0,0 +1,2 @@
+touch /etc/apt/sources.list.d/vbox.list
+sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian quantal contrib" >> /etc/apt/sources.list.d/vbox.list'
--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#Check if run as root
+
+#
+# Run initialization checks and requests
+#
+# check if running as sudo
 ROOT_UID="0"
 if [ "$UID" -ne "$ROOT_UID" ] ; then
 	echo "Please run as sudo (NOT root)!"
 	exit 1
 fi
-
 # get distribution
 echo -e "\e[33mSelect a distribution:\e[39m"
 select distribution in */; do test -n "$distribution" && break; echo ">>> Invalid Selection"; done
@@ -16,76 +19,89 @@ echo -e "\e[33mSelect a machine:\e[39m"
 select machine in */; do test -n "$machine" && break; echo ">>> Invalid Selection"; done
 cd ..
 
-# get packages folders
 
-# load all package install names, ppas, and configs from folder packages/
-echo -e "\e[33mLoading packages ...\e[39m"
-declare -A ppas
-declare -A packages
-ppas_id=0
-packages_id=0
-
-for f in $distribution*.cfg ; do
-	source $f
-
-	if [ "$ppa" ] ; then
-		ppas[$ppas_id]=$ppa
-		let ppas_id=$ppas_id+1
-	fi
-	if [ "$install" ] ; then
-		packages[$packages_id]=$install
-		let packages_id=$packages_id+1
-	fi
-	unset ppa
-	unset config
-done
-for f in $distribution$machine*.cfg ; do
-	source $f
-
-	if [ "$ppa" ] ; then
-		ppas[$ppas_id]=$ppa
-		let ppas_id=$ppas_id+1
-	fi
-	if [ "$install" ] ; then
-		packages[$packages_id]=$install
-		let packages_id=$packages_id+1
-	fi
-	unset ppa
-	unset config
-done
-echo ${#ppas[@]}
-
-read
-
-# import all loaded PPAs
-echo -e "\e[32mLoaded ${#packages[@]} package(s).\e[39m"
-echo ''
-echo ''
+#
+# Deal with ppa additions, and pre-update scripts
+#
+# Load ppas files
+echo -e "\e[33mLoading package libraries...\e[39m"
+declare -a ppas
+let i=0
+if [ -f $distribution$machine"ppas" ]; then
+	while IFS=$'\n' read -r line_data; do
+	    ppas[i]="${line_data}"
+	    ((++i))
+	done < $distribution$machine"ppas"
+fi [ -f $distribution"ppas" ]; then
+	while IFS=$'\n' read -r line_data; do
+	    ppas[i]="${line_data}"
+	    ((++i))
+	done < $distribution"ppas"
+fi
 if [ ${#ppas[@]} -ne 0 ]; then
 	echo -e "\e[33mConfirm trought the PPA addition\e[39m"
 
 	for ppa in ${ppas[@]}; do>&1
 		add-apt-repository $ppa
 	done
-	echo -e "\e[33mUpdating package libraries...\e[39m"
-	apt-get update > /dev/null
-	apt-get upgrade > /dev/null
-	echo -e "\e[32mFinished updating package libraries.\e[39m"
-	echo ''
-	echo ''
 fi
+# run pre-update scripts
+echo -e "\e[33mRunning pre-update scripts...\e[39m"
+if [ -f $distribution$machine"pre-update.sh" ]; then
+	source $distribution$machine"pre-update.sh"
+fi
+if [ -f $distribution$machine"pre-update.sh" ]; then
+	source $distribution "pre-update.sh"
+fi
+# update package libraries and upgrade
+echo -e "\e[33mUpdating package libraries and upgrading...\e[39m"
+#apt-get update > /dev/null
+#apt-get upgrade > /dev/null
 
-# install all loaded packages
+
+#
+# Deal with pre-install scripts, packages installation, post-install scripts, and package upgrade
+#
+# run pre-install scripts
+echo -e "\e[33mRunning pre-install scripts...\e[39m"
+if [ -f $distribution$machine"pre-install.sh" ]; then
+	source $distribution$machine"pre-install.sh"
+fi
+if [ -f $distribution$machine"pre-install.sh" ]; then
+	source $distribution "pre-install.sh"
+fi
+# Load package names
+echo -e "\e[33mLoading and installing packages...\e[39m"
+declare -a installs
+let i=0
+fi [ -f $distribution$machine"installs" ]; then
+	while IFS=$'\n' read -r line_data; do
+	    installs[i]="${line_data}"
+	    ((++i))
+	done < $distribution$machine"installs"
+fi
+fi [ -f $distribution"installs" ]; then
+	while IFS=$'\n' read -r line_data; do
+	    installs[i]="${line_data}"
+	    ((++i))
+	done < $distribution"installs"
+fi
+# install packages
+echo -e "\e[33mInstalling package(s)...\e[39m"
 if [ ${#packages[@]} -ne 0 ]; then
-	echo -e "\e[33mInstalling package(s)...\e[39m"
 
 	for i in ${packages[@]}; do
 		apt-get	install $i > /dev/null
 		echo -e "\e[96m$i\e[39m"
 	done
-
 	#apt-get upgrade > /dev/null
-	echo -e "\e[32mFinished installing package(s).\e[39m"
-	echo ''
-	echo ''
+fi
+
+# run post-install scripts
+echo -e "\e[33mRunning post-install scripts...\e[39m"
+if [ -f $distribution$machine"post-install.sh" ]; then
+	source $distribution$machine"post-install.sh"
+fi
+if [ -f $distribution$machine"post-install.sh" ]; then
+	source $distribution "post-install.sh"
 fi
