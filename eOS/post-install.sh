8a75b33d4e8690e57711d8a89cb88416cbd7e8c2
Pedro Rodrigues <prodrigues1990@gmail.com>
2015-09-28 19:50:15 +0100

    Added php5-gd, php5-curl, and php5-tidy

--- eOS/post-install.sh
+++ eOS/post-install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# install curl
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
+# start the the laptop-mode-tools
 tlp start
 
+# enable mod_rewrite on apache
+sudo a2enmod rewrite
+# copy pre-made php config file
+cp php.ini /etc/php5/apache2/php.ini
+# redirect apache document root to /var/www/
 sed -i '/DocumentRoot/c\        DocumentRoot /var/www' /etc/apache2/sites-enabled/000-default.conf 
 rm -rf /var/www
 mkdir /var/www
